3da00a670b03624f42b603801af330c468aa2225
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-31 10:19:36 -0400

    debug

--- modules/takeModules.py
+++ modules/takeModules.py
@@ -45,7 +45,7 @@ def dataLoad_CN(clean_dir, noisy_dir, val_ratio, speech_per_set, test_flag):
     #c_files = glob.glob(clean_dir + "/" + "*.wav")
     #n_files = glob.glob(noisy_dir + "/" + "*.wav")
     n_files = [os.path.join(noisy_dir, i) for i in fnames]
-    c_files = [os.path.join(clean_dir, f.split('-')[0]+'.wav') for f in fnames]
+    c_files = [os.path.join(clean_dir, f.split('-')[0]+'-clean-VOiCES.wav') for f in fnames]
 
     print("FILES:", n_files[:1], c_files[:1])
 
