1649316212f35f325a32e84aed28c89d78f97dd2
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-12 23:14:41 -0400

    add MOS to loss func

--- modules/myLossFunction.py
+++ modules/myLossFunction.py
@@ -6,8 +6,8 @@ def SDR(s, x, y, smos, xmos, thres=20):
     n = x - s
     d = x - y
     diff = s - y
-    #sSDR = 10.0 * torch.log10( (s**2.0).sum(1) / ((diff**2.0).sum(1) + 1e-8) )
+    sSDR = 10.0 * torch.log10( (s**2.0).sum(1) / ((diff**2.0).sum(1) + 1e-8) )
     diff = n - d
-    nSDR = (10.0 * torch.log10( (n**2.0).sum(1) / ((diff**2.0).sum(1) + 1e-8) )) + smos/xmos
-    loss = 0.5 * (-thres * torch.tanh( sSDR/thres ))
+    nSDR = 10.0 * torch.log10( (n**2.0).sum(1) / ((diff**2.0).sum(1) + 1e-8) )
+    loss = 0.5 * (-thres * torch.tanh( sSDR/thres ) - thres * torch.tanh( nSDR/thres ) ) + smos/xmos
     return loss.mean()
