3fa5d2feb97179088a59ff83057880d0c7a9384d
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-26 15:42:50 -0400

    debug

--- pyramid-mos/data.py
+++ pyramid-mos/data.py
@@ -77,7 +77,7 @@ def _collate_fn(batch):
 
     # w/o zero padding
     batch = batch[0]
-    batch_scores = batch[1]
+    batch_scores = batch[-1]
     print("bscores", batch_scores)
     aud_batch, aud_fnames_batch, mos_scores = load_mini_batch(batch)
 
