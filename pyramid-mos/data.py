13f254419d3a40473cd6135f28ae69ecb59b1a91
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-28 15:15:44 -0400

    debug

--- pyramid-mos/data.py
+++ pyramid-mos/data.py
@@ -49,7 +49,7 @@ def collate_custom(batch_data, maxlen=751):
         aud, _ = librosa.core.load(aud, sr=16000)
         aud = 10*np.log10(librosa.stft(aud, n_fft=512))
         lens.append(torch.tensor(aud.shape[1]))
-        aud = nn.ZeroPad2d(padding=(0, maxlen-aud.shape[1], 0, 0))(torch.tensor(aud))
+        aud = nn.ZeroPad2d(padding=(0, maxlen-aud.shape[1], 0, 0))(torch.tensor(aud, dtype=torch.cfloat))
         batch_aud.append(aud)
         batch_scores.append(torch.tensor(score))
     return {"aud":torch.stack(batch_aud), "lens": torch.stack(lens), "score":torch.stack(batch_scores)}
