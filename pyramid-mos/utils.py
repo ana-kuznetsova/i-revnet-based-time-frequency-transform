7d056ac4ca027120f5a876834fb9abb06bfdb183
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-28 16:31:31 -0400

    utils

--- pyramid-mos/utils.py
+++ pyramid-mos/utils.py
@@ -9,12 +9,16 @@ train = fnames[:int(len(fnames)*0.8)]
 test = fnames[int(len(fnames)*0.8):]
 
 stack = None
+MAXLEN = 0
 
 for f in tqdm(train):
     f, _ = librosa.core.load(f, sr=16000)
     f = np.nan_to_num(np.abs(10*np.log10(librosa.stft(f, n_fft=512))))
-    #MAXLEN = max(MAXLEN, f.shape[1])
+    MAXLEN = max(MAXLEN, f.shape[1])
+    print(MAXLEN)
 
+print("MAXLEN:", MAXLEN)
+'''
     if stack is None:
         stack = f 
     else:
@@ -37,3 +41,4 @@ for f in tqdm(test):
 
 #print("MAXLEN:", MAXLEN)
 print("Mean Test:", stack.mean(), "Std Test:", stack.std())
+'''
