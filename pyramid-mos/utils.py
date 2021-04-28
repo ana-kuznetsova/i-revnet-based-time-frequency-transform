097cd419312e7cf9beea1ef0d992bafb14831940
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-28 13:25:10 -0400

    maxlen

--- pyramid-mos/utils.py
+++ pyramid-mos/utils.py
@@ -6,14 +6,11 @@ import librosa
 
 fnames = pd.read_csv('/nobackup/anakuzne/data/COSINE-orig/csv/all.csv')['path']
 
-all_ = None
+MAXLEN = 0
 
-for i, f in tqdm(enumerate(fnames)):
-    f, _ = librosa.core.load(f, 16000)
-    f = 10*np.log10(librosa.stft(f, n_fft=512))
-    if i == 0:
-        all_ = f
-    else:
-        all_ = np.concatenate((all_,f), axis=1)
+for f in tqdm(fnames):
+    f, _ = librosa.core.load(f, sr=16000)
+    f = librosa.stft(f, n_fft=512)
+    MAXLEN = max(MAXLEN, f.shape[1])
 
-print("mean:", all_.mean(), "std:", all_.std())
+print("MAXLEN:", MAXLEN)
