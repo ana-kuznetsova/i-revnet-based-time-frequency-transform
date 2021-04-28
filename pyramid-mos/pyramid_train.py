8d1bb02379a3aa77e67b0dfa249bb5db7c92e4a1
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-28 14:39:06 -0400

    debug

--- pyramid-mos/pyramid_train.py
+++ pyramid-mos/pyramid_train.py
@@ -37,7 +37,8 @@ loader = data.DataLoader(dataset, batch_size=32, shuffle=False, collate_fn=colla
 for ep in range(1, epochs+1):
     for batch in loader:
         aud = batch['aud'].to(device)
-        lens = batch['lens'].to(device)
+        lens = batch['lens']
+        print(lens)
         scores = batch['score'].to(device)
         keys, values, lens = model(aud, lens)
         print(keys)
