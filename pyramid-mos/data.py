e003bd182277a116cf88d946546375ca80c5e190
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-29 18:59:11 -0400

    test

--- pyramid-mos/data.py
+++ pyramid-mos/data.py
@@ -77,3 +77,4 @@ if __name__ == "__main__":
 
     dataset_dev = Data(csv_path, mode='dev')
     loader_dev = data.DataLoader(dataset, batch_size=5, shuffle=False, collate_fn=collate_custom)
+    print(len(loader), len(loader_dev))
