6e875bd2f1d9ecdab652b33d378db4e904065d64
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-04-28 20:41:12 -0400

    dev

--- pyramid-mos/encoder.py
+++ pyramid-mos/encoder.py
@@ -31,6 +31,7 @@ class Encoder(nn.Module):
         x = torch.transpose(x, 1, -1)
         rnn_inp=pack_padded_sequence(x, lengths=lens, batch_first=True)
         outputs, _=self.lstm(rnn_inp)
+        print(outputs.data.shape)
         linear_input, _=pad_packed_sequence(outputs)
 
         for i in range(3):
