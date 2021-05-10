e14479dec1a529faa64a3613bab5f922d55901eb
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-09 22:27:16 -0400

    test data set

--- cross_val.py
+++ cross_val.py
@@ -29,36 +29,32 @@ class Data(data.Dataset):
         if self.mode=='test':
             return len(self.test)
 
-    def read_aud(self, clean_path, noisy_path):
-        clean, _ = librosa.core.load(clean_path, sr=16000)
-        noisy, _ = librosa.core.load(noisy_path, sr=16000)
-        res = np.concatenate([clean, noisy], axis=1)
-        return torch.tensor(res)
-
-
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         if self.mode=='train':
             clean = self.train[idx][0]
             noisy = self.train[idx][1]
-            res = self.read_aud(clean, noisy)
-            return res
+            return clean,  noisy
         if self.mode=='dev':
             clean = self.dev[idx][0]
             noisy = self.dev[idx][1]
-            res = self.read_aud(clean, noisy)
-            return res
+            return clean,  noisy
         if self.mode=='test':
             clean = self.test[idx][0]
             noisy = self.test[idx][1]
-            res = self.read_aud(clean, noisy)
-            return res
+            return clean,  noisy
 
+
+def collate_fn(clean_path, noisy_path):
+    clean, _ = librosa.core.load(clean_path, sr=16000)
+    noisy, _ = librosa.core.load(noisy_path, sr=16000)
+    res = np.concatenate([clean, noisy], axis=1)
+    return torch.tensor(res)
 
 dataset = Data(clean_path='/nobackup/anakuzne/data/COSINE-orig/clean-train',
                noisy_path='/nobackup/anakuzne/data/COSINE-orig/noisy-train', mode='train')
 
-loader = data.DataLoader(dataset, batch_size=None, shuffle=True, collate_fn=None)
+loader = data.DataLoader(dataset, batch_size=None, shuffle=True, collate_fn=collate_fn)
 for ex in loader:
     print(ex)
