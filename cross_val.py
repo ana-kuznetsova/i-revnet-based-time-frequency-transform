f300535d2bc7d0f69418ef8e25eff09d4672e8c8
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-11 13:42:21 -0400

    test

--- cross_val.py
+++ cross_val.py
@@ -94,7 +94,7 @@ def init_model(layerNum=6, filt ='UNet5SpecNorm',
 def train(model, optimizer, 
          criterion, train_loader, 
          val_loader=None, maxEpoch=500, 
-         batchSize=16, speechLen=2**15):
+         batchSize=16, speechLen=2**15, deviceNum=1):
     for epoch in range(1, maxEpoch+1):
         sumLoss  = 0.0
         sumSDR = 0.0
