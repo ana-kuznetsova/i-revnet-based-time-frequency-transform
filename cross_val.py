4bce9e3f0ae2c4c4f0e827e96922c9bab30cd749
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-05-11 13:14:43 -0400

    test

--- cross_val.py
+++ cross_val.py
@@ -12,53 +12,184 @@ import random
 
 
 class Data(data.Dataset):
-    def __init__(self, clean_path, noisy_path, mode='train'):
-        self.cnames = [os.path.join(clean_path, n) for n in os.listdir(clean_path)]
-        self.nnames = [os.path.join(noisy_path, n) for n in os.listdir(noisy_path)]
-        self.df = random.sample([(i, j) for i, j in zip(self.cnames, self.nnames)], len(self.cnames))
-        self.train = self.df[:int(len(self.df)*0.7)]
-        self.dev = self.df[int(len(self.df)*0.7):int(len(self.df)*0.8)]
-        self.test = self.df[int(len(self.df)*0.8):]
-        self.mode = mode
-    
+    def __init__(self, clean_paths, noisy_paths):
+        self.clean_paths = clean_paths
+        self.noisy_paths = noisy_paths
+        
     def __len__(self):
-        if self.mode=='train':
-            return len(self.train)
-        if self.mode=='dev':
-            return len(self.dev)
-        if self.mode=='test':
-            return len(self.test)
-
+        return len(self.clean_paths)
+    
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        if self.mode=='train':
-            clean = self.train[idx][0]
-            noisy = self.train[idx][1]
-            return clean,  noisy
-        if self.mode=='dev':
-            clean = self.dev[idx][0]
-            noisy = self.dev[idx][1]
-            return clean,  noisy
-        if self.mode=='test':
-            clean = self.test[idx][0]
-            noisy = self.test[idx][1]
-            return clean,  noisy
-
+        return (self.clean_paths[idx], self.noisy_paths[idx])
 
 def collate_fn(data):
     clean_path = data[0]
     noisy_path = data[1]
-    print(clean_path, noisy_path)
     clean, _ = librosa.core.load(clean_path, sr=16000)
     noisy, _ = librosa.core.load(noisy_path, sr=16000)
-    print(clean.shape, noisy.shape)
     res = np.concatenate([clean, noisy])
     return torch.tensor(res)
 
-dataset = Data(clean_path='/nobackup/anakuzne/data/COSINE-orig/clean-train',
-               noisy_path='/nobackup/anakuzne/data/COSINE-orig/noisy-train', mode='train')
+def crossvalid(model=None,criterion=None,optimizer=None,dataset=None,k_fold=5):
+    
+    train_score = pd.Series()
+    val_score = pd.Series()
+    
+    total_size = len(dataset)
+    fraction = 1/k_fold
+    seg = int(total_size * fraction)
+    # tr:train,val:valid; r:right,l:left;  eg: trrr: right index of right side train subset 
+    # index: [trll,trlr],[vall,valr],[trrl,trrr]
+    for i in range(k_fold):
+        trll = 0
+        trlr = i * seg
+        vall = trlr
+        valr = i * seg + seg
+        trrl = valr
+        trrr = total_size
 
-loader = data.DataLoader(dataset, batch_size=None, shuffle=True, collate_fn=collate_fn)
-for ex in loader:
-    print(ex)
+        train_left_indices = list(range(trll,trlr))
+        train_right_indices = list(range(trrl,trrr))
+        
+        train_indices = train_left_indices + train_right_indices
+        val_indices = list(range(vall,valr))
+        
+        train_set = torch.utils.data.dataset.Subset(dataset, train_indices)
+        val_set = torch.utils.data.dataset.Subset(dataset, val_indices)
+        
+
+        train_loader = torch.utils.data.DataLoader(train_set, batch_size=None, shuffle=True)
+
+        val_loader = torch.utils.data.DataLoader(val_set, batch_size=None, shuffle=True)
+        train_acc = train(res_model,criterion,optimizer,train_loader,epoch=1)
+        train_score.at[i] = train_acc
+        val_acc = valid(res_model,criterion,optimizer,val_loader)
+        val_score.at[i] = val_acc
+    
+    return train_score,val_score
+
+
+
+def train(model, train_loader):
+    for epoch in range(1, maxEpoch+1):
+        sumLoss  = 0.0
+        sumSDR = 0.0
+
+        for setNum in range(len(trainData)):
+            sys.stdout.write('\repoch: '+str(epoch)+' TrnSet: '+str(setNum+1)+'/'+str(len(trainData))) 
+            sys.stdout.flush()
+            
+            batchNum = len(train_loader)//batchSize
+            for utter in range(batchNum):
+                optimizer.zero_grad()
+                s = torch.from_numpy(np.array([])).float().reshape((0, speechLen)).cuda(deviceNum)
+                x = torch.from_numpy(np.array([])).float().reshape((0, speechLen)).cuda(deviceNum)
+                for bs in range(batchSize):
+                    stmp = trainMiniSet[perm2[bs+utter*batchSize]][0]
+                    xtmp = trainMiniSet[perm2[bs+utter*batchSize]][1]
+                    
+                    if len(stmp)>speechLen:
+                        st = np.random.randint(len(stmp)-speechLen)
+                        end = st+speechLen
+                        stmp = stmp[st:end]
+                        xtmp = xtmp[st:end]
+                    else:
+                        zLen = speechLen - len(stmp)
+                        tmpPad = torch.zeros((zLen)).cuda(deviceNum)
+                        stmp = torch.cat((stmp, tmpPad), 0)
+                        xtmp = torch.cat((xtmp, tmpPad), 0)
+                    
+                    stmp =  stmp.unsqueeze(0)
+                    xtmp =  xtmp.unsqueeze(0)
+                    s = torch.cat((s,stmp), 0)
+                    x = torch.cat((x,xtmp), 0)
+                y, _, _ = estClean(x)
+                loss = lossFunc(s, x, y, smos, xmos)
+                loss.backward()
+                optimizer.step()
+                
+                sumLoss += loss.detach().cpu().numpy()
+                    
+        sys.stdout.write('\n')
+        
+    print("time/epoch(Train):"+str(time.time() - start))
+    print("avg. loss:"+str(sumLoss/batchNum))
+    trainLoss= np.append(trainLoss, sumLoss/batchNum)
+    wandb.log({"loss": sumLoss/batchNum})
+    
+    if valRatio !=0:
+        start = time.time()
+        for param in estClean.parameters():
+            param.requires_grad = False
+        sumLoss_val  = 0.0
+        sumSDR_val = 0.0
+        perm1_val = np.random.permutation( len(validData) )
+        for setNum in range( len(validData) ):
+            sys.stdout.write('\repoch: '+str(epoch)+' ValSet: '+str(setNum+1)+'/'+str(len(validData))) 
+            sys.stdout.flush()
+            validMiniSet, valScoresMiniSet = tm.list_to_gpu( validData[ perm1_val[setNum] ], deviceNum )
+            perm2_val = np.random.permutation( len(validMiniSet) )
+            batchNum_val = len(validMiniSet)//batchSize
+            for utter in range(batchNum_val):
+                optimizer.zero_grad()
+                s_val = torch.from_numpy(np.array([])).float().reshape((0, speechLen)).cuda(deviceNum)
+                x_val = torch.from_numpy(np.array([])).float().reshape((0, speechLen)).cuda(deviceNum)
+                for bs in range( batchSize ):
+                    stmp = validMiniSet[perm2_val[bs+utter*batchSize]][0]
+                    xtmp = validMiniSet[perm2_val[bs+utter*batchSize]][1]
+                    smos = valScoresMiniSet[perm2_val[bs+utter*batchSize]][0]
+                    xmos = valScoresMiniSet[perm2_val[bs+utter*batchSize]][1]
+                    
+                    if len(stmp)>speechLen:
+                        st = np.random.randint(len(stmp)-speechLen)
+                        end = st+speechLen
+                        stmp = stmp[st:end]
+                        xtmp = xtmp[st:end]
+                    else:
+                        zLen = speechLen - len(stmp)
+                        tmpPad = torch.zeros((zLen)).cuda(deviceNum)
+                        stmp = torch.cat( (stmp, tmpPad), 0 )
+                        xtmp = torch.cat( (xtmp, tmpPad), 0 )
+                    
+                    stmp =  stmp.unsqueeze(0)
+                    xtmp =  xtmp.unsqueeze(0)
+                    s_val = torch.cat( (s_val,stmp), 0)
+                    x_val = torch.cat( (x_val,xtmp), 0)
+                    
+                s_val.detach()
+                x_val.detach()
+                y_val, _, _ = estClean(x_val)
+                d_val = x_val-y_val
+                n_val = x_val-s_val
+                loss = lossFunc(s_val, x_val, y_val, smos, xmos)
+                sumLoss_val += loss.detach().cpu().numpy()
+                    
+        sys.stdout.write('\n')
+        
+        print("time/epoch(Valid):"+str(time.time() - start))
+        print("avg. loss:"+str(sumLoss_val/batchNum_val))
+        validLoss= np.append(validLoss, sumLoss_val/batchNum_val)
+        wandb.log({"val_loss": sumLoss_val/batchNum_val})
+        for param in estClean.parameters():
+            param.requires_grad = True
+        
+if testFlag == 0: 
+    print('save DNN at epoch '+str(epoch))
+    torch.save(estClean.state_dict(), fileName+'_ep'+str(epoch))
+else:
+    print('test mode (do not save)') 
+
+train_score,val_score = crossvalid(res_model,criterion,optimizer,dataset=tiny_dataset)
+
+
+
+#####main func######
+
+dataset = Data(clean_path='/nobackup/anakuzne/data/COSINE-orig/clean-train',
+               noisy_path='/nobackup/anakuzne/data/COSINE-orig/noisy-train')
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=None, shuffle=True, collate_fn=collate_fn)
+
+for i in train_loader:
+    print(i)
